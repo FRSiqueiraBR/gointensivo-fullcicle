887758860f88d9e2288dccbc7979f3c0444f0db7
Felipe Siqueira <feliperdsiqueira@gmail.com>
2023-08-05 02:21:53 -0300

    feat: finalizando live dia 2

--- main.go
+++ main.go
@@ -2,32 +2,51 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 
 	"github.com/FRSiqueiraBR/gointensivo-fullcycle/internal/infra/database"
 	"github.com/FRSiqueiraBR/gointensivo-fullcycle/internal/usecase"
-	"github.com/satori/go.uuid"
+	"github.com/FRSiqueiraBR/gointensivo-fullcycle/pkg/rabbitmq"
 	_ "github.com/mattn/go-sqlite3"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	db, err := sql.Open("sqlite3", "db.sqlite3")
-
-	orderRepository := database.NewOrderRepository(db)
-	uc := usecase.NewCalculateFinalPrice(orderRepository)
-
-	myuuid := uuid.NewV4()
-
-	input := usecase.OrderInput{
-		ID: myuuid.String(),
-		Price: 10.0,
-		Tax: 1.0,
-	}
-
-	output, err := uc.Execute(input)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(output)
+	defer db.Close() //espera tudo rodar depois executa o close
+
+	orderRepository := database.NewOrderRepository(db)
+	uc := usecase.NewCalculateFinalPrice(orderRepository)
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+
+	defer ch.Close()
+	msgRabbitmqChannel := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, msgRabbitmqChannel) //escutando a fila // trava aqui // thread 2
+	rabbitmqWorker(msgRabbitmqChannel, uc)
+}
+
+func rabbitmqWorker(msgChan chan amqp.Delivery, uc *usecase.CalculateFinalPrice) {
+	fmt.Println("Start RabbitMQ")
+	for msg := range msgChan {
+		var input usecase.OrderInput
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			panic(err)
+		}
+		output, err := uc.Execute(input)
+		if err != nil {
+			panic(err)
+		}
+
+		msg.Ack(false)
+		fmt.Println("Mensagem processada e salva no banco: ", output)
+	}
 }
